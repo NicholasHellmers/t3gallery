1cc8a5679ad4de6004b300faf4f3e1b773683d2a
Nicholas Hellmers <nihe1661@colorado.edu>
2024-05-23 14:04:25 -0600

    DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import  { db } from "~/server/db";
 
 const mockURLS = [
   "https://utfs.io/f/8b91abcc-82c2-46e0-9897-705e5c080b57-p9gfu1.jpeg",
@@ -12,12 +13,19 @@ const mockImages = mockURLS.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main>
-      <div className="flex flex-wrap gap-5"> {
-        [...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48 p-4">
+      <div className="flex flex-wrap gap-5"> 
+        { posts.map((post) => (
+          <div key={post.id}> {post.name} </div>
+        ))}
+        {[...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 p-4">
             <img src={image.url} alt="image" />
           </div>
         ))}
