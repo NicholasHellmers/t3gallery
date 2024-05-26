610d4041462f44eb3ad671d925ced5776b7e791a
Nicholas Hellmers <nihe1661@colorado.edu>
2024-05-25 22:40:52 -0600

    some dynamic stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import  { db } from "~/server/db";
 
+export const dynamic = "force-dynamic"
+
 const mockURLS = [
   "https://utfs.io/f/8b91abcc-82c2-46e0-9897-705e5c080b57-p9gfu1.jpeg",
   "https://utfs.io/f/8b91abcc-82c2-46e0-9897-705e5c080b57-p9gfu1.jpeg",
